8fa4e309b779b52697727355da2f7fb533a6f5e1
Artur <artur@51degrees.com>
2023-05-09 16:18:33 +0100

    test

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -5,6 +5,7 @@ param(
     [hashtable]$Keys
 )
 
+Write-Host "-DSuperResourceKey=$($Keys.TestResourceKey)"
 ./java/run-unit-tests.ps1 -RepoName "location-java-test" -ProjectDir $ProjectDir -Name $Name -ExtraArgs "-DSuperResourceKey=$($Keys.TestResourceKey)"
 
 exit $LASTEXITCODE
