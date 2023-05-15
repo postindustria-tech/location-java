10488857d8a7094c4db785c5e96708e7f4d9b801
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-15 13:11:56 +0100

    Update run-unit-tests.ps1

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -2,9 +2,11 @@
 param(
     [string]$ProjectDir = ".",
     [string]$Name,
-    [hashtable]$Keys
+    [hashtable]$Keys,
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName
 )
 
-./java/run-unit-tests.ps1 -RepoName "location-java" -ProjectDir $ProjectDir -Name $Name -ExtraArgs "-DSuperResourceKey=$($Keys.TestResourceKey)"
+./java/run-unit-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -ExtraArgs "-DSuperResourceKey=$($Keys.TestResourceKey)"
 
 exit $LASTEXITCODE
